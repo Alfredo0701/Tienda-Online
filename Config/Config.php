65cc3ddb70a84bd335131739b95ce91da6e5926d
Alfredo0701 <141590256+Alfredo0701@users.noreply.github.com>
2024-11-21 17:22:47 -0600

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/shop/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "shop";
+const CHARSET = "charset=utf8";
+const TITLE = "SHOP";
+const MONEDA = "USD";
+const CLIENT_ID = "AeOQt6U6EBDIPo9KdbloNPfo1buJlkYocDqWQtAQ3kdYLULZoWu3l-2zvUI8VSPfQlMVFhUmrfSjxVpQ";
+?>
